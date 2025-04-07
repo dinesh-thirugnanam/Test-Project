f02dc553e26309513f05593dcf09b3e952fa087f
Dinesh <gtdinesh05@gmail.com>
2025-04-07 16:57:09 +0530

    Added something

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Don't Read our docs
           </a>
         </div>
       </main>
